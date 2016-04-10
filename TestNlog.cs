f88844bf2e8a227ba77b9c8e4a9c3c512b1f8f38
minhhoangvn <hnminh@outlook.com>
2016-04-10 10:49:30 +0700

    Test build log with NLog

---
+++ TestNlog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NLog;
+
+namespace AutomationFrameWork
+{
+    class TestNlog
+    {
+        static Logger logger = LogManager.GetCurrentClassLogger();
+        [Test]
+        public void TesTNlog ()
+        {
+           
+            for (int n = 0; n < 10; n++)
+            {
+                logger.Debug("This write to log " + n);
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+    }
+}
