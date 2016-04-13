521c0db26511e3f3e1b84282beb496799c8d4547
minhhoangvn <hnminh@outlook.com>
2016-04-13 23:27:27 +0700

    .

--- TestNlog.cs
+++ TestNlog.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using NUnit.Framework;
 using NLog;
 using AutomationFrameWork.Base;
+using System.Net;
+using System.Net.NetworkInformation;
+
 namespace AutomationFrameWork
 {
     [TestFixture][ReportManager]
@@ -34,5 +37,47 @@ namespace AutomationFrameWork
         {
             Console.WriteLine(" tear down in test");
         }
+        [Test]
+        public void GetPort ()
+        {
+            List<int> temp=GetAvailablePort(10);
+            foreach (int port in temp)
+                Console.WriteLine("Port: " + port +" is free");
+           
+        }
+        public static List<int> GetAvailablePort (int startingPort)
+        {
+            IPEndPoint[] endPoints;
+            List<int> portArray = new List<int>();
+            List<int> returnPort=new List<int>();
+            IPGlobalProperties properties = IPGlobalProperties.GetIPGlobalProperties();
+
+            //getting active connections
+            TcpConnectionInformation[] connections = properties.GetActiveTcpConnections();
+            portArray.AddRange(from n in connections
+                               where n.LocalEndPoint.Port >= startingPort
+                               select n.LocalEndPoint.Port);
+
+            //getting active tcp listners - WCF service listening in tcp
+            endPoints = properties.GetActiveTcpListeners();
+            portArray.AddRange(from n in endPoints
+                               where n.Port >= startingPort
+                               select n.Port);
+
+            //getting active udp listeners
+            endPoints = properties.GetActiveUdpListeners();
+            portArray.AddRange(from n in endPoints
+                               where n.Port >= startingPort
+                               select n.Port);
+
+            portArray.Sort();
+
+            for (int i = startingPort; i < UInt16.MaxValue; i++)
+                if (!portArray.Contains(i))
+                    returnPort.Add(i);
+
+            return returnPort;
+        }
     }
+   
 }
