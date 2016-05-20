602380c4c35d019e695bd725bb6417d7c50a1031
minh <hnminh@outlook.com>
2016-05-20 09:31:53 +0700

    .

--- AutomationTesting/UnitTest1.cs
+++ AutomationTesting/UnitTest1.cs
@@ -14,6 +14,7 @@ using OpenQA.Selenium.IE;
 using System.Threading;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Chrome;
+using AutomationFrameWork.ActionsKeys;
 
 namespace AutomationTesting
 { 
@@ -29,6 +30,16 @@ namespace AutomationTesting
             Console.WriteLine(c);
             Console.WriteLine(d);           
             Console.WriteLine("============================");                      
+        }
+        [Test]
+        public void Ex3()
+        {
+            DriverFactory.Instance.StartDriver(DriverType.Chrome);
+            WebKeywords.Instance.Navigate("http://www.kbb.com");			
+            string _pageSource = DriverFactory.Instance.GetWebDriver.PageSource;
+            DriverFactory.Instance.CloseDriver();                       
+            foreach (Match _match in Regex.Matches(_pageSource, "dataVersionId.+[0-9]+\""))
+                Console.WriteLine(_match.Value);  
         }       
     }
 }
