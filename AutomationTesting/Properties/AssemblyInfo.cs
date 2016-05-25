79f59c0999a02b975582ca033364ec0f20a7b32e
minhhoangvn <hnminh@outlook.com>
2016-05-26 00:07:29 +0700

    Add TestListener Modify Click WebKeywords

--- AutomationTesting/Properties/AssemblyInfo.cs
+++ AutomationTesting/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using NUnit.Framework;
+using NUnit.Engine.Extensibility;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
