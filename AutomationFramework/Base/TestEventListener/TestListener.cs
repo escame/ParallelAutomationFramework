74da19f4da232a2030c0316297ea551bb6ebe061
minhhoangvn <hnminh@outlook.com>
2016-05-26 22:05:30 +0700

    Config for ITestEventListener

---
+++ AutomationFramework/Base/TestEventListener/TestListener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Engine;
+using NUnit.Engine.Extensibility;
+
+namespace AutomationFrameWork.Base.TestEventListener
+{
+    
+    [Extension(Path = "/NUnit/Engine/TypeExtensions/ITestEventListener")]
+    public class TestListener : ITestEventListener
+    {
+        StringBuilder _builder = new StringBuilder();
+        public void OnTestEvent (string report)
+        {
+
+            WriteText(report);
+
+        }
+        private void WriteText (string text)
+        {
+            _builder.AppendLine();
+            _builder.Append(text);
+            System.IO.File.WriteAllText(@"C:\\Test\\WriteLines.txt", _builder.ToString());
+        }
+        
+    }
+}
---
+++ AutomationTesting/TestITestListener.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using AutomationFrameWork.Base.TestEventListener;
+using System.Runtime.CompilerServices;
+using NUnit.Engine;
+namespace AutomationTesting
+{
+   
+   
+    public class TestITestListener 
+    {
+       
+        [Test]
+        public void Event ()
+        {
+            Console.WriteLine();
+        }
+    }
+}
