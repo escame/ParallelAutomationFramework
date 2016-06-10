97e728c3e909e209b9fd544e0be618d67a1b25a3
minhhoangvn <hnminh@outlook.com>
2016-06-10 18:33:19 +0700

    Create Branch IDrivers

---
+++ AutomationFramework/Base/Driver/Interface/IDrivers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationFrameWork.Driver
+{
+    public interface IDrivers
+    {
+        object StartDriver(object driverServices,object desiredCapabilities,TimeSpan commandTimeOut);
+        void CloseDriver();
+    }
+}
